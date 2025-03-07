cf86191b0cdd3b4cce12d432b18e0b5393b0ac62
Zhenyu Li <zhenyu009@e.ntu.edu.sg>
2025-03-07 15:01:04 +0800

    Update Bert-Base-Chinese-Uncertainty-demo.py

--- Bert-Base-Chinese-Uncertainty-demo.py
+++ Bert-Base-Chinese-Uncertainty-demo.py
@@ -1,12 +1,7 @@
 # -*- coding: utf-8 -*-
-"""
-Created on Tue May 14 15:58:00 2024
-
-@author: ShowerLee
-"""
+# Note: the following code is for demonstration purpose. Please use GPU for fast inference on large scale dataset.
 
 from transformers import BertTokenizer, BertForSequenceClassification, pipeline
-# tested in transformers==4.18.0 
 import transformers
 transformers.__version__
 
@@ -14,8 +9,8 @@ transformers.__version__
 if __name__ == "__main__":
 
     bert = BertForSequenceClassification.from_pretrained('./bert-base-chinese-uncertainty',num_labels=2)
-    tokenizer = BertTokenizer.from_pretrained('./vocab.txt')
+    tokenizer = BertTokenizer.from_pretrained('google-bert/bert-base-chinese')
     nlp = pipeline("text-classification", model=bert, tokenizer=tokenizer)
-
     results = nlp(['牛顿研究发现物体所受到的外力等于动量对时间的一阶导数。',
                    '现有研究表明物体所受到的外力等于动量对时间的一阶导数。'])
+    results
